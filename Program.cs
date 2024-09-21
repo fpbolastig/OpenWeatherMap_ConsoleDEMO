4316a1bbce81fd6689fb880648962c69ddd0358d
Dexter Bolastig <fpbolastig@hotmail.com>
2024-09-22 03:47:11 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Xml.Linq;
+
+namespace OpenWeatherMap_ConsoleDEMO
+{
+    class Program
+    {
+        private static readonly string APIKey = "bc97e27f7ab7edd5abafb980b7301d19"; 
+        private static readonly HttpClient client = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Input a City (ex. Manila): ");
+            string sCity = Console.ReadLine();
+
+            Console.WriteLine("\nPlease wait...");
+            await GetWeather_Json(sCity);
+            await GetWeather_XML(sCity);
+        }
+
+        private static async Task GetWeather_Json(string city)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}";
+            var resp = await client.GetStringAsync(url);
+            var weatherData = JObject.Parse(resp);
+
+            Console.WriteLine("\nWeather Data (JSON Format):");
+            Console.WriteLine(weatherData.ToString());
+        }
+
+        private static async Task GetWeather_XML(string city)
+        {
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={APIKey}&mode=xml";
+            var resp = await client.GetStringAsync(url);
+            var weatherData = XDocument.Parse(resp);
+
+            Console.WriteLine("\nWeather Data (XML Format):");
+            Console.WriteLine(weatherData.ToString());
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,21 @@
+﻿Project Name: DEMO Open Weather Map
+Version: 1.0.0.0 [BASE]
+Description: A demo project that integrates an OpenWeatherMap API in a console application. 
+	It fetches weather data in both JSON and XML formats upon supply of name of a city ex. Manila.
+Author: Frederick Bolastig
+
+History:
+A request to create a DEMO console project in C# that integrate with the API stated in https://openweathermap.org/current, integration each for the JSON and XML formats.
+
+Side Note:
+This is the basic form of the project as per instruction for Demo purpose only. Any additional processes are not included 
+e.a. error checking, validations.
+
+Requires internet connection to access API.
+
+Visit and create an account at https://openweathermap.org to generate APIKey.
+
+HowTo:
+1.The program prompts the user to input a city name.
+2.the program will sends requests (one for JSon, and another for XML) to the OpenWeatherMap API.
+3. It parses and prints the data for both formats.
